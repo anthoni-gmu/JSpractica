098637a07b17cfd235999d1bac55e2a1216436c5
anthoni-gmu <softwaretoni21@gmail.com>
2021-03-25 02:24:09 -0500

    maquetacion

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import Variables from './variables.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const variables = new Variables()
+
+
+    console.log(variables.mostrar())
+    console.log(variables.suma())
+
+})
